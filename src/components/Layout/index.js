f286d17a6ddcb59abb9ebb48150a798cff3346c5
Thandeka Zulu <tsazulu@yahoo.com>
2023-07-19 15:15:55 +0200

    Version 1

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router-dom';
 import Sidebar from '../Sidebar';
 import './index.scss';
 
@@ -9,8 +10,7 @@ const Layout = () => {
     <div className='page'>
       <span className='tags top-tags'>&lt;body&gt;</span>
       
-      {/* <Oulet /> */}
-
+      <Outlet />
       <span className='tags bottom-tags'>
       &lt;/body&gt;
       <br/>
