1f733311e61f8003ddad2b3b1785acd5cff31452
Thandeka Zulu <tsazulu@yahoo.com>
2023-07-19 10:37:45 +0200

    .

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,14 +1,40 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome,faUser,faGithub } from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => (
   <div className='nav-bar'>
     <Link className='logo' to='/'>
       <img src={LogoS} alt="logo" />
-      <img src={LogoSubtitle} alt="slobodan" />
+      <img className='sub-logo' src={LogoSubtitle} alt="slobodan" />
     </Link>
+    <nav>
+      <NavLink exact='true' activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      </NavLink>
+
+      <NavLink exact='true' activeclassname="active" className="about-link" to="/about">
+        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+      </NavLink>
+
+      <NavLink exact='true' activeclassname="active" className="contact-link" to="/contact">
+        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+      </NavLink>
+    </nav>
+    <ul>
+      <li>
+        <a 
+          target='_blank' 
+          rel='noreferrer' 
+          href='linkedin.com/in/angel-mazulu-b68645265'>
+          <FontAwesomeIcon icon="fa-brands fa-linkedin" size="lg" />
+          {/* <FontAwesomeIcon icon={faGithub} color='#4d4d4e' /> */}
+        </a>
+      </li>
+    </ul>
   </div>
 )
 
