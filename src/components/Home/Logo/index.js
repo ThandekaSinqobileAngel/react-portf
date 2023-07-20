e7c63a141c54590615a3449d4fb1ccaa6ad700b5
Thandeka Zulu <tsazulu@yahoo.com>
2023-07-20 12:11:04 +0200

    Version 1

---
+++ src/components/Home/Logo/index.js
@@ -0,0 +1,16 @@
+import './index.scss';
+import LogoS from '../../../assets/images/logo-s.png'
+
+const Logo = () => {
+  return (
+    <div className='logo-container'>
+      <img className='solid-logo' src='{LogoS' alt='S' />
+      <svg
+        width='559pt'
+        height='897pt'
+        
+    </div>
+  )
+}
+
+export default Logo;
