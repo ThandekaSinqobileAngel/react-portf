6303da940b72c2914b2884f9372431f17dcc8771
Thandeka Zulu <tsazulu@yahoo.com>
2023-07-20 13:38:20 +0200

    Version 1

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,11 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo-s.png';
 // import { Loader } from '@react-three/drei';
-// import Logo from './Logo'
-import './index.scss';
-import { useEffect, useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
+import Logo from './Logo'
+import './index.scss';
+
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -45,7 +46,7 @@ const Home = () => {
             CONTACT ME
           </Link>
         </div>
-
+      <Logo/>
       </div>
     </>
   )
